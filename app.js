95d096bd8bcdfd1f472f0a5ed4ad4a5d10e4aba2
zakariyahya <yazakkars@gmail.com>
2022-09-08 16:07:12 +0700

    assignment 3 express js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 app.get('/about', (req,res)=>{
-    res.sendFile(__dirname + '/contact.html')
+    res.sendFile(__dirname + '/about.html')
 })
 app.get('/contact', (req,res)=>{
     res.sendFile(__dirname + '/contact.html')
